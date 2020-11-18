f10d99b2d15b097cccb62b950fa6e04db05bf66d
Brian Bouwman <bbouwma1@my.westga.edu>
2020-11-18 10:08:55 -0500

    First Merge Commit -- Models added

---
+++ TeamANumbrix/TeamANumbrix/Model/Puzzle.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testPuzzle.Model
+{
+    /// <summary>
+    /// The grid class
+    /// </summary>
+    /// <seealso cref="System.Collections.Generic.ICollection{testPuzzle.Model.Cell}" />
+    public class Puzzle : ICollection<Cell>
+    {
+        /// <summary>
+        /// Gets the cells.
+        /// </summary>
+        /// <value>
+        /// The cells.
+        /// </value>
+        private IList<Cell> Cells { get; }
+
+        /// <summary>
+        /// Gets the number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1"></see>.
+        /// </summary>
+        public int Count => this.Cells.Count;
+
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="T:System.Collections.Generic.ICollection`1"></see> is read-only.
+        /// </summary>
+        public bool IsReadOnly => true;
+
+        public Puzzle()
+        {
+            this.Cells = new Collection<Cell>();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// An enumerator that can be used to iterate through the collection.
+        /// </returns>
+        public IEnumerator<Cell> GetEnumerator()
+        {
+            return this.Cells.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Adds the specified cell.
+        /// </summary>
+        /// <param name="cell">The cell.</param>
+        /// <exception cref="NullReferenceException">Cell cannot be null</exception>
+        public void Add(Cell cell)
+        {
+            if (cell == null)
+            {
+                throw new NullReferenceException("Cell cannot be null");
+            }
+        }
+
+        /// <summary>
+        /// Removes all items from the <see cref="T:System.Collections.Generic.ICollection`1"></see>.
+        /// </summary>
+        public void Clear()
+        {
+            this.Cells.Clear();
+        }
+
+        /// <summary>
+        /// Determines whether this instance contains the object.
+        /// </summary>
+        /// <param name="cell">The cell.</param>
+        /// <returns>
+        ///   <c>true</c> if [contains] [the specified cell]; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="NullReferenceException">Item cannot be null</exception>
+        public bool Contains(Cell cell)
+        {
+            var containsCell = false;
+            if (cell == null)
+            {
+                throw new NullReferenceException("Cell cannot be null");
+            }
+
+            if (this.Cells.Contains(cell))
+            {
+                containsCell = true;
+            }
+
+            return containsCell;
+        }
+
+        /// <summary>
+        /// Copies the elements of the <see cref="T:System.Collections.Generic.ICollection`1"></see> to an <see cref="T:System.Array"></see>, starting at a particular <see cref="T:System.Array"></see> index.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="T:System.Array"></see> that is the destination of the elements copied from <see cref="T:System.Collections.Generic.ICollection`1"></see>. The <see cref="T:System.Array"></see> must have zero-based indexing.</param>
+        /// <param name="arrayIndex">The zero-based index in array at which copying begins.</param>
+        /// <exception cref="ArgumentException">Index cannot be negative</exception>
+        public void CopyTo(Cell[] array, int arrayIndex)
+        {
+            if (arrayIndex < 0)
+            {
+                throw new ArgumentException("Index cannot be negative");
+            }
+
+            this.Cells.CopyTo(array, arrayIndex);
+        }
+
+        /// <summary>
+        /// Removes the specified cell.
+        /// </summary>
+        /// <param name="cell">The cell.</param>
+        /// <returns></returns>
+        /// <exception cref="NullReferenceException">Statistic cannot be null</exception>
+        public bool Remove(Cell cell)
+        {
+            if (cell == null)
+            {
+                throw new NullReferenceException("Cell cannot be null");
+            }
+
+            return this.Cells.Remove(cell);
+        }
+
+        /// <summary>
+        /// Adds all.
+        /// </summary>
+        /// <param name="cells">The cells.</param>
+        /// <exception cref="NullReferenceException">Cells cannot be null</exception>
+        public void AddAll(IEnumerable<Cell> cells)
+        {
+            if (cells == null)
+            {
+                throw new NullReferenceException("Cells cannot be null");
+            }
+
+            foreach (var currentCell in cells)
+            {
+                this.Cells.Add(currentCell);
+            }
+        }
+
+    }
+}
---
+++ TeamANumbrix/TeamANumbrix/Model/cell.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testPuzzle.Model
+{
+    public class Cell : IComparable<Cell>
+    {
+        /// <summary>
+        /// Gets or sets the position.
+        /// </summary>
+        /// <value>
+        /// The position.
+        /// </value>
+        public int Position { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public int Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance has n.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance has n; otherwise, <c>false</c>.
+        /// </value>
+        public bool HasN { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance has s.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance has s; otherwise, <c>false</c>.
+        /// </value>
+        public bool HasS { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance has e.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance has e; otherwise, <c>false</c>.
+        /// </value>
+        public bool HasE { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance has w.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance has w; otherwise, <c>false</c>.
+        /// </value>
+        public bool HasW { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is changeable.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is changeable; otherwise, <c>false</c>.
+        /// </value>
+        public bool isChangeable { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cell"/> class.
+        /// </summary>
+        /// <param name="position">The position.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="hasN">if set to <c>true</c> [has n].</param>
+        /// <param name="hasS">if set to <c>true</c> [has s].</param>
+        /// <param name="hasE">if set to <c>true</c> [has e].</param>
+        /// <param name="hasW">if set to <c>true</c> [has w].</param>
+        /// <exception cref="ArgumentException">
+        /// position cannot be less than 0
+        /// or
+        /// value cannot be less than 0
+        /// </exception>
+        public Cell(int position, int value, bool hasN, bool hasS, bool hasE, bool hasW)
+        {
+            if (position < 0)
+            {
+                throw new ArgumentException("position cannot be less than 0");
+            }
+
+            if (value < 0)
+            {
+                throw new ArgumentException("value cannot be less than 0");
+            }
+
+            this.Position = position;
+            this.Value = value;
+            this.HasN = hasN;
+            this.HasS = hasS;
+            this.HasE = hasE;
+            this.HasW = hasW;
+            this.isChangeable = true;
+        }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return $"Cell has position : {this.Position}";
+        }
+
+        /// <summary>
+        /// Compares to.
+        /// </summary>
+        /// <param name="otherCell">The other cell.</param>
+        /// <returns></returns>
+        public int CompareTo(Cell otherCell)
+        {
+            return this.Position.CompareTo(otherCell.Position);
+        }
+    }
+}
