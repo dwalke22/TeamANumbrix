bec1d5b57b65b355e2cd7296f187154c084e59c4
David Walker <thewalker.david@gmail.com>
2020-11-17 20:27:19 -0500

    Removed a redundancie

--- TeamANumbrix/TeamANumbrix/View/MainPage.xaml.cs
+++ TeamANumbrix/TeamANumbrix/View/MainPage.xaml.cs
@@ -9,7 +9,7 @@ namespace TeamANumbrix.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
         public const int ApplicationHeight = 750;
 
