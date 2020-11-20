5fbede9f392dc1c32e767501678cf8f500a71e8e
David Walker <thewalker.david@gmail.com>
2020-11-19 20:52:13 -0500

    Cleaned up Puzzle.cs

--- TeamANumbrix/TeamANumbrix/Model/Puzzle.cs
+++ TeamANumbrix/TeamANumbrix/Model/Puzzle.cs
@@ -4,60 +4,72 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 
-
 namespace TeamANumbrix.Model
 {
     /// <summary>
-    /// The grid class
+    ///     The grid class
     /// </summary>
     /// <seealso cref="Cell" />
     public class Puzzle : ICollection<Cell>
     {
+        #region Properties
+
         /// <summary>
-        /// Gets the cells.
+        ///     Gets the cells.
         /// </summary>
         /// <value>
-        /// The cells.
+        ///     The cells.
         /// </value>
         private ICollection<Cell> Cells { get; }
 
         /// <summary>
-        /// Gets the number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1"></see>.
+        ///     Gets the number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1"></see>.
         /// </summary>
         public int Count => this.Cells.Count;
 
         /// <summary>
-        /// Gets or sets the size of the dimension.
+        ///     Gets or sets the size of the dimension.
         /// </summary>
         /// <value>
-        /// The size of the dimension.
+        ///     The size of the dimension.
         /// </value>
         public int DimensionSize { get; set; }
 
         /// <summary>
-        /// Gets a value indicating whether the <see cref="T:System.Collections.Generic.ICollection`1"></see> is read-only.
+        ///     Gets a value indicating whether the <see cref="T:System.Collections.Generic.ICollection`1"></see> is read-only.
         /// </summary>
         public bool IsReadOnly => true;
 
         /// <summary>
-        /// Gets the indexed cells.
+        ///     Gets the indexed cells.
         /// </summary>
         /// <value>
-        /// The indexed cells.
+        ///     The indexed cells.
         /// </value>
         public List<Cell> IndexedCells => this.Cells.ToList();
 
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        ///     Instantiates a new Puzzle object
+        /// </summary>
+        /// <param name="dimensionSize"></param>
         public Puzzle(int dimensionSize)
         {
             this.Cells = new Collection<Cell>();
             this.DimensionSize = dimensionSize;
         }
 
+        #endregion
+
+        #region Methods
+
         /// <summary>
-        /// Returns an enumerator that iterates through the collection.
+        ///     Returns an enumerator that iterates through the collection.
         /// </summary>
         /// <returns>
-        /// An enumerator that can be used to iterate through the collection.
+        ///     An enumerator that can be used to iterate through the collection.
         /// </returns>
         public IEnumerator<Cell> GetEnumerator()
         {
@@ -70,7 +82,7 @@ namespace TeamANumbrix.Model
         }
 
         /// <summary>
-        /// Adds the specified cell.
+        ///     Adds the specified cell.
         /// </summary>
         /// <param name="cell">The cell.</param>
         /// <exception cref="NullReferenceException">Cell cannot be null</exception>
@@ -83,7 +95,7 @@ namespace TeamANumbrix.Model
         }
 
         /// <summary>
-        /// Removes all items from the <see cref="T:System.Collections.Generic.ICollection`1"></see>.
+        ///     Removes all items from the <see cref="T:System.Collections.Generic.ICollection`1"></see>.
         /// </summary>
         public void Clear()
         {
@@ -91,11 +103,11 @@ namespace TeamANumbrix.Model
         }
 
         /// <summary>
-        /// Determines whether this instance contains the object.
+        ///     Determines whether this instance contains the object.
         /// </summary>
         /// <param name="cell">The cell.</param>
         /// <returns>
-        ///   <c>true</c> if [contains] [the specified cell]; otherwise, <c>false</c>.
+        ///     <c>true</c> if [contains] [the specified cell]; otherwise, <c>false</c>.
         /// </returns>
         /// <exception cref="NullReferenceException">Item cannot be null</exception>
         public bool Contains(Cell cell)
@@ -115,9 +127,14 @@ namespace TeamANumbrix.Model
         }
 
         /// <summary>
-        /// Copies the elements of the <see cref="T:System.Collections.Generic.ICollection`1"></see> to an <see cref="T:System.Array"></see>, starting at a particular <see cref="T:System.Array"></see> index.
+        ///     Copies the elements of the <see cref="T:System.Collections.Generic.ICollection`1"></see> to an
+        ///     <see cref="T:System.Array"></see>, starting at a particular <see cref="T:System.Array"></see> index.
         /// </summary>
-        /// <param name="array">The one-dimensional <see cref="T:System.Array"></see> that is the destination of the elements copied from <see cref="T:System.Collections.Generic.ICollection`1"></see>. The <see cref="T:System.Array"></see> must have zero-based indexing.</param>
+        /// <param name="array">
+        ///     The one-dimensional <see cref="T:System.Array"></see> that is the destination of the elements
+        ///     copied from <see cref="T:System.Collections.Generic.ICollection`1"></see>. The <see cref="T:System.Array"></see>
+        ///     must have zero-based indexing.
+        /// </param>
         /// <param name="arrayIndex">The zero-based index in array at which copying begins.</param>
         /// <exception cref="ArgumentException">Index cannot be negative</exception>
         public void CopyTo(Cell[] array, int arrayIndex)
@@ -131,7 +148,7 @@ namespace TeamANumbrix.Model
         }
 
         /// <summary>
-        /// Removes the specified cell.
+        ///     Removes the specified cell.
         /// </summary>
         /// <param name="cell">The cell.</param>
         /// <returns></returns>
@@ -147,7 +164,7 @@ namespace TeamANumbrix.Model
         }
 
         /// <summary>
-        /// Adds all.
+        ///     Adds all.
         /// </summary>
         /// <param name="cells">The cells.</param>
         /// <exception cref="NullReferenceException">Cells cannot be null</exception>
@@ -165,7 +182,7 @@ namespace TeamANumbrix.Model
         }
 
         /// <summary>
-        /// Removes all.
+        ///     Removes all.
         /// </summary>
         /// <param name="cells">The cells.</param>
         /// <exception cref="NullReferenceException">Cells cannot be null</exception>
@@ -182,5 +199,6 @@ namespace TeamANumbrix.Model
             }
         }
 
+        #endregion
     }
 }
